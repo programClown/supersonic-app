3549a4f2242aa8a107c942e115a0accbb064ca70
codeCLown <zhaomengshou@126.com>
2024-12-24 08:43:28 +0800

    迁客骚人，多会于此

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,14 +1,10 @@
 <template>
-  <title-bar />
-  <div class="main">
     <ElConfigProvider :locale="elementLocale">
       <RouterView />
     </ElConfigProvider>
-  </div>
 </template>
 
 <script setup lang="ts">
-import TitleBar from '@/components/title-bar/title-bar.vue'
 import { elementLocale } from '@/locales'
 import { useElementPlusDesignTokens } from '@vben/effects/hooks'
 import { ElConfigProvider } from 'element-plus'
@@ -17,10 +13,3 @@ defineOptions({ name: 'App' })
 
 useElementPlusDesignTokens()
 </script>
-
-<style scoped>
-.main {
-  padding-top: 30px;
-  box-sizing: border-box;
-}
-</style>
